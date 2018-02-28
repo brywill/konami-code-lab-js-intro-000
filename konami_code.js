3bc38e7c051493443ad26a5ed1cd93d30e6613a9
Bryan Williams <brywilldev@gmail.com>
2018-02-28 00:59:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   let index = 0;
 
-  var body = document.getElementByTagName('body');
+  const body = document.querySelector('body');
 
   body.addEventListener('keydown', function(e) {
     const key = parseInt(e.which);
