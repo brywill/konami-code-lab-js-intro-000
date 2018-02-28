6be287034a61338d7c4fdfc6635d4996864d681e
Bryan Williams <brywilldev@gmail.com>
2018-02-28 00:37:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,10 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  console.log(e.detail, e.which, e.location)
+  document.addEventListener('keydown', function(event) {
+    console.log(e.detail, e.which, e.location)
+  })
+  
 
 }
 init()
