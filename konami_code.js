33822980eb6beed64e414e9d09a25efad49f24ff
Bryan Williams <brywilldev@gmail.com>
2018-02-28 00:44:49 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,10 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
+  let index = 0;
+
   document.addEventListener('keydown', function(e) {
-    console.log(e.detail, e.which, e.location)
-  })
+    const key = parseInt(e.which);
+    if (key === code[index]) {
+      index++;
 
-
+      if (index === code.length) {
+        alert("Nice!");
+        index = 0;
+      } else {
+        index = 0;
+      }
+    }
+  });
 }
 init()
